967bf0772dfc2f610954b75801335672ddf4c2b0
princemaxwell2011 <ajoseph379@gmail.com>
2024-12-22 16:14:11 +0100

    first commit

---
+++ operator_1.py
@@ -0,0 +1,20 @@
+name = "Dammzy"
+age=24
+height= 30.28
+isstudent=True
+print("My name is ", name )
+print("The data type of is " ,type(name))
+print("The data type of age is" ,type(age))
+print("The data type of height is" ,type(height))
+print("The data type of isstudent is",type(isstudent))
+print("I am ", age)
+print("Am of an average height of", height)
+print("Aftertypecasting")
+age1=str(age)
+print(age1)
+print("The data type of age is", age1)
+height1=int(height)
+print(height1)
+print("The data type of age is", height)
+
+
---
+++ string Operation.py
@@ -0,0 +1,14 @@
+first_name = "Codingal"
+last_name = "Education"
+full_name = first_name+last_name
+example = "Haa"*5
+
+print("First Name :", first_name)
+print("Last Name :", last_name)
+print("Full Name :", full_name)
+print("string Multiplied 5 times gives the result :",example)
+word= "coding"
+print("length of string :",len(word))
+print("first Letter of string :", word[0])
+print("Last Letter of String:",word[5])
+print("String Sliced :", word)
